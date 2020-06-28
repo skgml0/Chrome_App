ec7d78e56eb9456cbb72b8630bd949a7c2427a7e
skgml0 <hnh2714@gmail.com>
2020-06-28 12:00:00 +0900

    greeting+setFile

--- app2.js
+++
@@ -1,9 +0,0 @@
-const loginForm = document.querySelector("#login-form");
-const loginInput = document.querySelector("#login-form input");
-
-function onLoginSubmit(event) {
-  event.preventDefault();
-  console.log(loginInput.value);
-}
-
-loginForm.addEventListener("submit", onLoginSubmit);
