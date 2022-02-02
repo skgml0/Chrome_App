4853a9460d44ff9670ffea73397b48f48eb9610f
skgml0 <hnh2714@gmail.com>
2022-02-02 12:01:00 +0900

    [add] body에 bgImg로 추가

--- js/background.js
+++ js/background.js
@@ -1,6 +1,8 @@
-const images = ["0.jpg", "1.jpg", "2.jpg", "3.jpg"];
+const images = ["1.jpg", "2.jpg", "3.jpg"];
 const chosenImg = images[Math.floor(Math.random() * images.length)];
-const bgImage = document.createElement("img");
-bgImage.src = `img/${chosenImg}`;
+document.body.style.backgroundImage = `url(img/${chosenImg})`;
+// img 태그 생성시
+// const bgImage = document.createElement("img");
+// bgImage.src = `img/${chosenImg}`;
 // document.body.appendChild(bgImage);
-document.body.prepend(bgImage);
+// document.body.prepend(bgImage);
