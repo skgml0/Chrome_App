739d5b03473c2c0d3f5cfd64934a16155cef69b9
skgml0 <hnh2714@gmail.com>
2022-02-03 19:46:31 +0900

    [add] 버튼 모양(flag)

--- js/todo.js
+++ js/todo.js
@@ -22,7 +22,7 @@ function paintToDo(newTodo) {
   const span = document.createElement("span");
   span.innerText = newTodo.text;
   const button = document.createElement("button");
-  button.innerText = "X";
+  button.innerText = "🚩";
   button.addEventListener("click", deleteToDo);
   li.appendChild(span);
   li.appendChild(button);
