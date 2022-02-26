3e89f67cfdc8e82cce40d341db50a1ec30229cde
skgml0 <hnh2714@gmail.com>
2022-02-26 19:11:16 +0900

    🚚  querySelector 이름 변경

--- js/greetings.js
+++ js/greetings.js
@@ -1,5 +1,5 @@
 const loginForm = document.querySelector("#login-form");
-const loginInput = document.querySelector("#login-form input");
+const loginInput = document.querySelector("#login-form .name-input");
 const greeting = document.querySelector("#greeting");
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
